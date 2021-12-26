8adbd3fcf809736579cb1ac936e8d72aab8b4e28
girish-ramakrishnan <81526867+girish-ramakrishnan@users.noreply.github.com>
2021-12-26 10:03:15 -0500

    Update helloworld.java
    Updated new System out change

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,6 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World from Java again1");
+    System.out.println("Hello World from Polled by Jenkins for Changes");
+    System.out.println("Hello World from Test Automation");
     }
 }
